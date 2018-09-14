2baf45d5aa52e447065ebec6f5f4b4f7a8dbaa7a
gongxun <gongxun@xiaomi.com>
2018-09-14 23:29:41 +0800

    添加sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from app.models.book import db
 from app.web import web
 from app.config import Config
 
@@ -7,6 +8,9 @@ from app.config import Config
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    db.init_app(app)
+    db.create_all(app=app)
 
     register_blueprint(app)
     return app
--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@
 class Config(object):
     DEBUG = False
     SECRET_KEY = 'fisher'
+    SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:root@localhost:3306/fisher'
 
     #     custom
     PER_PAGE = 15
